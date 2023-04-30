7350359d603936dca81444b76921e23f620ea9a8
Sibgha Zahid <sibghazahid@macs-MacBook-Pro.local>
2023-05-01 04:53:13 +0500

    calendar

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -6,7 +6,6 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Bluetooth from '../screens/bluetooth/Bluetooth';
 import Scan from '../screens/bluetooth/Scan';
 import BluetoothDevices from '../screens/bluetooth/BluetoothDevices';
-import PhysicalActivities from '../screens/physicalActivities/PhysicalActivities';
 const Stack = createNativeStackNavigator();
 const BluetoothStack = () => {
   return (
@@ -21,7 +20,7 @@ const BluetoothStack = () => {
 const MainStackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="DrawerNavigator" component={PhysicalActivities}  />
+      <Stack.Screen name="DrawerNavigator" component={DrawerNavigator}  />
       <Stack.Screen name="Notifications" component={Notifications} />
       <Stack.Screen name="BluetoothStack" component={BluetoothStack} />
 
